36be9cb4a30967bda79f78a1e9929be7bfaf4f21
Christopher Alley <calleyviola@gmail.com>
2023-08-18 13:20:38 -0400

    Add global variables for DOM elements

--- js/main.js
+++ js/main.js
@@ -6,13 +6,17 @@ const $body = $("body");
 
 const $storiesLoadingMsg = $("#stories-loading-msg");
 const $allStoriesList = $("#all-stories-list");
+const $favoriteStoriesList = $("#favorite-stories-list");
 
 const $loginForm = $("#login-form");
 const $signupForm = $("#signup-form");
+const $storyForm = $("#story-form");
 
 const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
+const $navSubmit = $("#nav-submit");
+const $navFavorites = $("#nav-favorites");
 
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
